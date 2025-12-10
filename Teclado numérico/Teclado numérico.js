7b9923f5b808d85a76382f118054f8398030d5d9
aarongarrido7e8-design <aaron.garrido.7e8@itb.cat>
2025-12-10 16:28:27 +0100

    Actualización Teclado numérico: Añadido más colores y nuevo código JS

---
+++ Teclado numérico/Teclado numérico.js
@@ -0,0 +1,49 @@
+//Cojo el div del teclado, para luego ir añadiendo
+let teclado = document.getElementById("teclado");
+
+for (let i = 1; i < 101; i++) {
+    //Creo la tecla
+    let tecla = document.createElement("div");
+    tecla.innerHTML = "<p>" + i + "</p>";
+    tecla.className = "tecla";
+
+    if ((i % 2) == 0) {
+        //poner fondo azul
+        tecla.style.background = "blue";
+        tecla.style.color = "white";
+    }
+
+    if ((i % 5) == 0) {
+        //poner fondo azul
+        tecla.style.background = "yellow";
+        tecla.style.color = "black";
+    }
+
+    if ((i % 3) == 0) {
+        //poner fondo rojo
+        tecla.style.background = "red";
+        tecla.style.color = "white";
+    }
+
+if (esPrimo(i)) {
+        //poner fondo verde
+        tecla.style.background = "green";
+        tecla.style.color = "white";
+    }
+
+
+    //Añado la tecla al teclado
+    teclado.appendChild(tecla);
+}
+
+function esPrimo(num) {
+    if (num < 2) return false;        // 0 y 1 no son primos
+    let limite = Math.sqrt(num);      // optimización
+
+    for (let i = 2; i <= limite; i++) {
+        if (num % i === 0) {
+            return false;             // si es divisible, no es primo
+        }
+    }
+    return true;                       // si no tuvo divisores, es primo
+}
